2afbdcead76dc2863e26e386150b11ccc36ec3fd
xuefan.chen <xuefan.chen@ck-telecom.com>
2016-03-11 21:29:12 +0800

    this is first commit

---
+++ CMakeFiles/videoStabdemo.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/videoStabdemo.dir/videostab.cpp.o"
+  "videoStabdemo.pdb"
+  "videoStabdemo"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/videoStabdemo.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
---
+++ README.md
@@ -0,0 +1,1 @@
+This project is just for study. Maybe you can get the help on the website http://nghiaho.com/?p=2093
